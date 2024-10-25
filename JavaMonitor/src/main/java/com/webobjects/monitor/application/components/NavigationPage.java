12300bd8bd545863d3d04bb57ac523e7db9e9919
Hugi Thordarson <hugi@karlmenn.is>
2024-10-25 04:42:17 +0000

    Smoe nice icons

--- JavaMonitor/src/main/java/com/webobjects/monitor/application/components/NavigationPage.java
+++ JavaMonitor/src/main/java/com/webobjects/monitor/application/components/NavigationPage.java
@@ -95,13 +95,14 @@ public class NavigationPage extends MonitorComponent {
 	 */
 	public List<MenuItem> menuItems() {
 		final ArrayList<MenuItem> items = new ArrayList<>();
-		items.add( new MenuItem( "Applications", Icon.Home, this::ApplicationsPageClicked ) );
-		items.add( new MenuItem( "Hosts", Icon.Home, this::HostsPageClicked ) );
+		items.add( new MenuItem( "Applications", Icon.Cube, this::ApplicationsPageClicked ) );
+		items.add( new MenuItem( "Hosts", Icon.Server, this::HostsPageClicked ) );
 		items.add( new MenuItem( "Site", Icon.Home, this::ConfigurePageClicked ) );
-		items.add( new MenuItem( "Preferences", Icon.Home, this::PrefsPageClicked ) );
+		items.add( new MenuItem( "Preferences", Icon.Adjustments, this::PrefsPageClicked ) );
+		items.add( new MenuItem( "Help", Icon.Help, this::HelpPageClicked ) );
 
 		if( showModProxyTab() ) {
-			items.add( new MenuItem( "mod_proxy", Icon.Home, this::ModProxyPageClicked) );
+			items.add( new MenuItem( "mod_proxy", Icon.Polygon, this::ModProxyPageClicked) );
 		}
 
 		if( logoutRequired() ) {
@@ -117,12 +118,27 @@ public class NavigationPage extends MonitorComponent {
 	public record MenuItem( String name, Icon icon, Supplier<WOComponent> supplier ) {}
 
 	/**
-	 * SVG icons we can use for our menuitems
+	 * SVG icons we can use for our menuitems. hijacked from https://tabler.io/icons
 	 */
 	public enum Icon {
+		Cube( """
+				<svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-cube"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M21 16.008v-8.018a1.98 1.98 0 0 0 -1 -1.717l-7 -4.008a2.016 2.016 0 0 0 -2 0l-7 4.008c-.619 .355 -1 1.01 -1 1.718v8.018c0 .709 .381 1.363 1 1.717l7 4.008a2.016 2.016 0 0 0 2 0l7 -4.008c.619 -.355 1 -1.01 1 -1.718z" /><path d="M12 22v-10" /><path d="M12 12l8.73 -5.04" /><path d="M3.27 6.96l8.73 5.04" /></svg>
+				"""),
+		Polygon("""
+				<svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-polygon"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M12 5m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0" /><path d="M19 8m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0" /><path d="M5 11m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0" /><path d="M15 19m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0" /><path d="M6.5 9.5l3.5 -3" /><path d="M14 5.5l3 1.5" /><path d="M18.5 10l-2.5 7" /><path d="M13.5 17.5l-7 -5" /></svg>
+				"""),
+		Help("""
+				<svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-help"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M12 12m-9 0a9 9 0 1 0 18 0a9 9 0 1 0 -18 0" /><path d="M12 17l0 .01" /><path d="M12 13.5a1.5 1.5 0 0 1 1 -1.5a2.6 2.6 0 1 0 -3 -4" /></svg>
+				"""),
 		Home( """
 				<svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M5 12l-2 0l9 -9l9 9l-2 0" /><path d="M5 12v7a2 2 0 0 0 2 2h10a2 2 0 0 0 2 -2v-7" /><path d="M9 21v-6a2 2 0 0 1 2 -2h2a2 2 0 0 1 2 2v6" /></svg>
-			""" );
+			""" ),
+		Adjustments("""
+				<svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-adjustments"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M4 10a2 2 0 1 0 4 0a2 2 0 0 0 -4 0" /><path d="M6 4v4" /><path d="M6 12v8" /><path d="M10 16a2 2 0 1 0 4 0a2 2 0 0 0 -4 0" /><path d="M12 4v10" /><path d="M12 18v2" /><path d="M16 7a2 2 0 1 0 4 0a2 2 0 0 0 -4 0" /><path d="M18 4v1" /><path d="M18 9v11" /></svg>
+				"""),
+		Server("""
+				<svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-server"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M3 4m0 3a3 3 0 0 1 3 -3h12a3 3 0 0 1 3 3v2a3 3 0 0 1 -3 3h-12a3 3 0 0 1 -3 -3z" /><path d="M3 12m0 3a3 3 0 0 1 3 -3h12a3 3 0 0 1 3 3v2a3 3 0 0 1 -3 3h-12a3 3 0 0 1 -3 -3z" /><path d="M7 8l0 .01" /><path d="M7 16l0 .01" /></svg>
+				""");
 
 		// SVG path for the icon
 		String _svg;
