931124decc8d20b9aac6b7162e154b6759ec0cc9
Hugi Thordarson <hugi@karlmenn.is>
2024-11-02 21:19:52 +0000

    Moved MHost.sendRequestToWotaskdArray() to WOTaskdHandler

--- JavaMonitorFramework/src/main/java/com/webobjects/monitor/_private/MHost.java
+++ JavaMonitorFramework/src/main/java/com/webobjects/monitor/_private/MHost.java
@@ -14,7 +14,6 @@ package com.webobjects.monitor._private;
 
 import java.net.InetAddress;
 import java.net.UnknownHostException;
-import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -24,12 +23,10 @@ import com.webobjects.appserver.WOHTTPConnection;
 import com.webobjects.appserver.WORequest;
 import com.webobjects.appserver.WOResponse;
 import com.webobjects.foundation.NSArray;
-import com.webobjects.foundation.NSData;
 import com.webobjects.foundation.NSDictionary;
 import com.webobjects.foundation.NSLog;
 import com.webobjects.foundation.NSMutableArray;
 import com.webobjects.foundation.NSMutableDictionary;
-import com.webobjects.foundation._NSThreadsafeMutableArray;
 
 import er.extensions.foundation.ERXProperties;
 
@@ -246,102 +243,6 @@ public class MHost extends MObject {
 		if( aValue != null ) {
 			processorType = aValue.toString();
 		}
-	}
-
-	/**
-	 * Communications Goop
-	 */
-	public static WOResponse[] sendRequestToWotaskdArray( String contentString, List<MHost> wotaskdArray, boolean willChange ) {
-
-		final MHost aHost = wotaskdArray.get( 0 );
-
-		// FIXME: A little danger sign here... // Hugi 2024-11-02
-		if( aHost == null ) {
-			return null;
-		}
-
-		final MSiteConfig aConfig = aHost.siteConfig();
-
-		// we had errors reaching a host last time - do it again!
-		if( aConfig.hostErrorArray.count() > 0 ) {
-			_syncRequest = null;
-			final WORequest aSyncRequest = syncRequest( aConfig );
-			final _NSThreadsafeMutableArray syncHosts = aConfig.hostErrorArray;
-
-			if( NSLog.debugLoggingAllowedForLevelAndGroups( NSLog.DebugLevelDetailed, NSLog.DebugGroupDeployment ) ) {
-				NSLog.debug.appendln( "Sending sync requests to: " + syncHosts.array() );
-			}
-
-			// final MSiteConfig finalConfig = aConfig;
-			final Thread[] workers = new Thread[syncHosts.count()];
-
-			for( int i = 0; i < workers.length; i++ ) {
-				final int j = i;
-
-				final Runnable work = new Runnable() {
-					@Override
-					public void run() {
-						MHost aHost = (MHost)syncHosts.objectAtIndex( j );
-						aHost.sendRequestToWotaskd( aSyncRequest, true, true );
-					}
-				};
-
-				workers[j] = new Thread( work );
-				workers[j].start();
-			}
-
-			try {
-				for( int i = 0; i < workers.length; i++ ) {
-					workers[i].join();
-				}
-			}
-			catch( InterruptedException ie ) {}
-		}
-
-		final WORequest aRequest = new WORequest( MObject._POST, MObject.directActionString, MObject._HTTP1, aConfig.passwordDictionary(), new NSData( contentString.getBytes() ), null );
-		final List<MHost> finalWotaskdArray = wotaskdArray;
-		final boolean wc = willChange;
-
-		final Thread[] workers = new Thread[wotaskdArray.size()];
-		final WOResponse[] responses = new WOResponse[workers.length];
-
-		for( int i = 0; i < workers.length; i++ ) {
-			final int j = i;
-
-			Runnable work = new Runnable() {
-				@Override
-				public void run() {
-					responses[j] = finalWotaskdArray.get( j ).sendRequestToWotaskd( aRequest, wc, false );
-				}
-			};
-
-			workers[j] = new Thread( work );
-			workers[j].start();
-		}
-
-		try {
-			for( int i = 0; i < workers.length; i++ ) {
-				workers[i].join();
-			}
-		}
-		catch( InterruptedException ie ) {
-			// might be bad?
-		}
-
-		return responses;
-	}
-
-	private static WORequest _syncRequest = null;
-
-	private static WORequest syncRequest( MSiteConfig aConfig ) {
-		if( _syncRequest == null ) {
-			final NSMutableDictionary<String, NSDictionary> data = new NSMutableDictionary<>( aConfig.dictionaryForArchive(), "SiteConfig" );
-			final NSMutableDictionary<String, NSMutableDictionary<String, NSDictionary>> updateWotaskd = new NSMutableDictionary<String, NSMutableDictionary<String, NSDictionary>>( data, "sync" );
-			final NSMutableDictionary<String, NSMutableDictionary<String, NSMutableDictionary<String, NSDictionary>>> monitorRequest = new NSMutableDictionary<String, NSMutableDictionary<String, NSMutableDictionary<String, NSDictionary>>>( updateWotaskd, "updateWotaskd" );
-			final NSData content = new NSData( (new CoderWrapper()).encodeRootObjectForKey( monitorRequest, "monitorRequest" ) );
-			_syncRequest = new WORequest( MObject._POST, MObject.directActionString, MObject._HTTP1, aConfig.passwordDictionary(), content, null );
-		}
-		return _syncRequest;
 	}
 
 	private String errorResponse = null;
