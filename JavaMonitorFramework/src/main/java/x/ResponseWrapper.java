8e683e2b233b21b9e601b926834ccca818dfb055
Hugi Thordarson <hugi@karlmenn.is>
2024-11-06 09:59:05 +0000

    ResponseWrapper no longer needs a WOResponse to wrap

--- JavaMonitorFramework/src/main/java/x/ResponseWrapper.java
+++ JavaMonitorFramework/src/main/java/x/ResponseWrapper.java
@@ -1,35 +1,15 @@
 package x;
 
-import com.webobjects.appserver.WOResponse;
-
 /**
  * A little wrapper class for us to use while we're wrapping up WO style responses
  */
 
 public class ResponseWrapper {
 
-	public WOResponse _woResponse;
 	public byte[] _content;
 
-	public WOResponse woResponse() {
-		return _woResponse;
-	}
-
 	public byte[] content() {
-
-		if( _content != null ) {
-			return _content;
-		}
-
-		if( _woResponse == null ) {
-			return null;
-		}
-
-		if( _woResponse.content() == null ) {
-			return null;
-		}
-
-		return _woResponse.content().bytes();
+		return _content;
 	}
 
 	public String contentString() {
@@ -38,14 +18,6 @@ public class ResponseWrapper {
 			return new String( _content );
 		}
 
-		if( _woResponse == null ) {
-			return null;
-		}
-
-		if( _woResponse.contentString() == null ) {
-			return null;
-		}
-
-		return _woResponse.contentString();
+		return null;
 	}
 }
