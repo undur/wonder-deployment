7d7cb341803ef84991842c52f7e9437d362b2606
Hugi Thordarson <hugi@karlmenn.is>
2024-11-09 09:54:18 +0000

    Start cleanup in MObject

--- JavaMonitorFramework/src/main/java/com/webobjects/monitor/_private/MObject.java
+++ JavaMonitorFramework/src/main/java/com/webobjects/monitor/_private/MObject.java
@@ -69,20 +69,25 @@ public class MObject /* implements NSKeyValueCoding */ {
 		_siteConfig.dataHasChanged();
 	}
 
-	public static Integer validatedInteger( Integer value ) {
+	public static Integer validatedInteger( final Integer value ) {
+
 		if( value == null ) {
-			return value;
+			return null;
 		}
+
 		return Integer.valueOf( Math.abs( value.intValue() ) );
 	}
 
 	public static Integer validatedUrlVersion( Integer version ) {
+
 		if( version != null ) {
 			int intVal = version.intValue();
+
 			if( intVal != 3 && intVal != 4 ) {
 				return Integer.valueOf( 4 );
 			}
 		}
+
 		return version;
 	}
 
@@ -98,14 +103,18 @@ public class MObject /* implements NSKeyValueCoding */ {
 	}
 
 	public static String validatedOutputPath( String value ) {
-		if( (value == null) || (value.length() == 0) ) {
+
+		if( value == null || value.length() == 0 ) {
 			return "/dev/null";
 		}
+
 		return value;
 	}
 
 	public static Integer validatedLifebeatInterval( Integer value ) {
+
 		int intVal = 0;
+
 		try {
 			intVal = value.intValue();
 		}
@@ -114,33 +123,37 @@ public class MObject /* implements NSKeyValueCoding */ {
 		if( intVal < 1 ) {
 			return Integer.valueOf( 30 );
 		}
+
 		return value;
 	}
 
 	public static String validatedSchedulingType( String value ) {
+
 		if( value != null ) {
-			if( (value.equals( "HOURLY" )) ||
-					(value.equals( "DAILY" )) ||
-					(value.equals( "WEEKLY" )) ) {
+			if( (value.equals( "HOURLY" )) || (value.equals( "DAILY" )) || (value.equals( "WEEKLY" )) ) {
 				return value;
 			}
 		}
+
 		return null;
 	}
 
 	public static Integer validatedSchedulingStartTime( Integer value ) {
+
 		if( value != null ) {
 			int intVal = value.intValue();
-			if( (intVal >= 0) && (intVal <= 23) ) {
+
+			if( intVal >= 0 && intVal <= 23 ) {
 				return value;
 			}
 		}
+
 		return null;
 	}
 
 	// Our array is from 0-23, but the display is for '12 AM' to '11 PM'
 	public static Integer morphedSchedulingStartTime( String value ) {
-		int i = MObject.TIMES_OF_DAY.indexOf( value );
+		int i = TIMES_OF_DAY.indexOf( value );
 
 		if( i != -1 ) {
 			return Integer.valueOf( i );
@@ -150,29 +163,31 @@ public class MObject /* implements NSKeyValueCoding */ {
 	}
 
 	public static String morphedSchedulingStartTime( Integer value ) {
+
 		if( value != null ) {
-			Object aString = MObject.TIMES_OF_DAY.get( value.intValue() );
-			if( aString != null ) {
-				return aString.toString();
-			}
+			return TIMES_OF_DAY.get( value.intValue() );
 		}
+
 		return null;
 	}
 
 	public static Integer validatedSchedulingStartDay( Integer value ) {
+
 		if( value != null ) {
 			int intVal = value.intValue();
-			if( (intVal >= 0) && (intVal <= 6) ) {
+
+			if( intVal >= 0 && intVal <= 6 ) {
 				return value;
 			}
 		}
+
 		return null;
 	}
 
 	// Java normally returns 1-7, ObjC returned 0-6, JavaFoundation will return 0-6
 	// Our array is from 0-6
 	public static Integer morphedSchedulingStartDay( String value ) {
-		int i = MObject.WEEKDAYS.indexOf( value );
+		int i = WEEKDAYS.indexOf( value );
 
 		if( i != -1 ) {
 			return Integer.valueOf( i );
@@ -182,13 +197,11 @@ public class MObject /* implements NSKeyValueCoding */ {
 	}
 
 	public static String morphedSchedulingStartDay( Integer value ) {
-		if( value != null ) {
-			Object aString = MObject.WEEKDAYS.get( value.intValue() );
 
-			if( aString != null ) {
-				return aString.toString();
-			}
+		if( value != null ) {
+			return WEEKDAYS.get( value.intValue() );
 		}
+
 		return null;
 	}
 
