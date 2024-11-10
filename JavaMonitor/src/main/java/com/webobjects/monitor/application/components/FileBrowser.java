6e985eb056c188ddf4f5154dee95860218c2a4b2
Hugi Thordarson <hugi@karlmenn.is>
2024-11-10 06:30:30 +0000

    Replace dicts/plists in FileBrowser/RemoteBrowser with actual types

--- JavaMonitor/src/main/java/com/webobjects/monitor/application/components/FileBrowser.java
+++ JavaMonitor/src/main/java/com/webobjects/monitor/application/components/FileBrowser.java
@@ -1,5 +1,9 @@
 package com.webobjects.monitor.application.components;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
 import com.webobjects.appserver.WOApplication;
 /*
  © Copyright 2006- 2007 Apple Computer, Inc. All rights reserved.
@@ -35,15 +39,20 @@ import com.webobjects.monitor.util.WOTaskdHandler;
 
 public class FileBrowser extends MonitorComponent {
 
+	public record RemoteResult( boolean isRoots, String filepath, List<RemoteFile> files ) {}
+	public record RemoteFile( String file, Integer fileSize, String fileType ) {}
+
 	public String startingPath; // passed in
 	public String callbackUpdateAction; // passed in
 	public String callbackSelectionAction; // passed in
 	public MHost host; // passed in
+
 	public boolean showFiles = true;
 	public boolean isRoots = false;
 	public String errorMsg;
-	public NSDictionary aCurrentFile;
-	public NSArray _fileList = null;
+
+	public RemoteFile aCurrentFile;
+	public List<RemoteFile> _fileList;
 
 	public FileBrowser( WOContext aWocontext ) {
 		super( aWocontext );
@@ -53,7 +62,7 @@ public class FileBrowser extends MonitorComponent {
 		return errorMsg != null && errorMsg.length() > 0;
 	}
 
-	public NSArray fileList() {
+	public List<RemoteFile> fileList() {
 		if( _fileList == null ) {
 			retrieveFileList();
 		}
@@ -63,10 +72,10 @@ public class FileBrowser extends MonitorComponent {
 
 	private String retrieveFileList() {
 		try {
-			NSDictionary aDict = RemoteBrowseClient.fileListForStartingPathHost( startingPath, host, showFiles );
-			_fileList = (NSArray)aDict.valueForKey( "fileArray" );
-			isRoots = (aDict.valueForKey( "isRoots" ) != null) ? true : false;
-			startingPath = (String)aDict.valueForKey( "filepath" );
+			RemoteResult fetched = RemoteBrowseClient.fileListForStartingPathHost( startingPath, host, showFiles );
+			_fileList = fetched.files();
+			isRoots = fetched.isRoots();
+			startingPath = fetched.filepath();
 			errorMsg = null;
 		}
 		catch( MonitorException me ) {
@@ -86,11 +95,7 @@ public class FileBrowser extends MonitorComponent {
 	}
 
 	public boolean isCurrentFileDirectory() {
-		String aString = (String)aCurrentFile.valueForKey( "fileType" );
-		if( aString.equals( "NSFileTypeDirectory" ) ) {
-			return true;
-		}
-		return false;
+		return aCurrentFile.fileType().equals( "NSFileTypeDirectory" );
 	}
 
 	public Object backClicked() {
@@ -108,7 +113,7 @@ public class FileBrowser extends MonitorComponent {
 
 	public Object directoryClicked() {
 		String originalPath = startingPath;
-		String aFile = (String)aCurrentFile.valueForKey( "file" );
+		String aFile = aCurrentFile.file();
 		startingPath = NSPathUtilities.stringByAppendingPathComponent( startingPath, aFile );
 		startingPath = NSPathUtilities._standardizedPath( startingPath );
 		retrieveFileList();
@@ -129,7 +134,7 @@ public class FileBrowser extends MonitorComponent {
 	}
 
 	public Object selectClicked() {
-		String aFile = (String)aCurrentFile.valueForKey( "file" );
+		String aFile = aCurrentFile.file();
 		startingPath = NSPathUtilities.stringByAppendingPathComponent( startingPath, aFile );
 		startingPath = NSPathUtilities._standardizedPath( startingPath );
 		return performParentAction( callbackSelectionAction );
@@ -142,48 +147,50 @@ public class FileBrowser extends MonitorComponent {
 
 	private static class RemoteBrowseClient {
 
+		// FIXME: Dear lord. I feel the pain of whoever wrote this // Hugi 2024-11-10
 		private static byte[] evilHack = "<?xml version=\"1.0\" encoding=\"ISO-8859-1\"?>".getBytes();
 
-		private static NSDictionary _getFileListOutOfResponse( WOResponse aResponse, String thePath ) throws MonitorException {
-			NSData responseContent = aResponse.content();
-			NSArray anArray = NSArray.EmptyArray;
+		private static RemoteResult _getFileListOutOfResponse( final WOResponse response, final String sourcePath ) throws MonitorException {
+			final NSData responseContent = response.content();
+			NSArray<NSDictionary> anArray = NSArray.EmptyArray;
+
 			if( responseContent != null ) {
-				byte[] responseContentBytes = responseContent.bytes();
-				String responseContentString = new String( responseContentBytes );
+				final byte[] responseContentBytes = responseContent.bytes();
+				final String responseContentString = new String( responseContentBytes );
 
 				if( responseContentString.startsWith( "ERROR" ) ) {
-					throw new MonitorException( "Path " + thePath + " does not exist" );
+					throw new MonitorException( "Path " + sourcePath + " does not exist" );
 				}
 
-				CoderWrapper aDecoder = new CoderWrapper();
-
 				try {
-					byte[] evilHackCombined = new byte[responseContentBytes.length + evilHack.length];
-					// System.arraycopy(src, src_pos, dst, dst_pos, length);
+					final byte[] evilHackCombined = new byte[responseContentBytes.length + evilHack.length];
 					System.arraycopy( evilHack, 0, evilHackCombined, 0, evilHack.length );
 					System.arraycopy( responseContentBytes, 0, evilHackCombined, evilHack.length, responseContentBytes.length );
-					anArray = (NSArray)aDecoder.decodeRootObject( evilHackCombined );
+					anArray = (NSArray)new CoderWrapper().decodeRootObject( evilHackCombined );
 				}
 				catch( WOXMLException wxe ) {
 					NSLog.err.appendln( "RemoteBrowseClient _getFileListOutOfResponse Error decoding response: " + responseContentString );
-					throw new MonitorException( "Host returned bad response for path " + thePath, wxe );
+					throw new MonitorException( "Host returned bad response for path " + sourcePath, wxe );
 				}
-
 			}
 			else {
 				NSLog.err.appendln( "RemoteBrowseClient _getFileListOutOfResponse Error decoding null response" );
-				throw new MonitorException( "Host returned null response for path " + thePath );
+				throw new MonitorException( "Host returned null response for path " + sourcePath );
 			}
 
-			String isRoots = aResponse.headerForKey( "isRoots" );
-			String filepath = aResponse.headerForKey( "filepath" );
+			final String isRoots = response.headerForKey( "isRoots" );
+			final String filepath = response.headerForKey( "filepath" );
 
-			NSMutableDictionary aDict = new NSMutableDictionary();
-			aDict.takeValueForKey( isRoots, "isRoots" );
-			aDict.takeValueForKey( filepath, "filepath" );
-			aDict.takeValueForKey( anArray, "fileArray" );
+			final RemoteResult remoteResult = new RemoteResult( isRoots != null, filepath, new ArrayList<>() );
 
-			return aDict;
+			for( Map<String,Object> map : anArray ) {
+				final String file = (String)map.get( "file" );
+				final Integer fileSize = (Integer)map.get( "fileSize" );
+				final String fileType = (String)map.get( "fileType" );
+				remoteResult.files().add( new RemoteFile( file, fileSize, fileType ) );
+			}
+
+			return remoteResult;
 		}
 
 		private static String getPathString = "/cgi-bin/WebObjects/wotaskd.woa/wa/RemoteBrowse/getPath";
@@ -191,41 +198,39 @@ public class FileBrowser extends MonitorComponent {
 		/**
 		 * FIXME: Switch to java http client // Hugi 2024-11-01
 		 */
-		public static NSDictionary fileListForStartingPathHost( String aString, MHost aHost, boolean showFiles ) throws MonitorException {
+		public static RemoteResult fileListForStartingPathHost( final String path, final MHost host, final boolean showFiles ) throws MonitorException {
 
-			NSDictionary aFileListDictionary = null;
+			RemoteResult aFileListDictionary = null;
 
 			try {
-				Application theApplication = (Application)WOApplication.application();
-				WOHTTPConnection anHTTPConnection = new WOHTTPConnection( aHost.name(), theApplication.lifebeatDestinationPort() );
-				@SuppressWarnings("cast")
-				NSMutableDictionary<String, NSMutableArray<String>> aHeadersDict = (NSMutableDictionary<String, NSMutableArray<String>>)WOTaskdHandler.siteConfig().passwordDictionary().mutableClone();
-				WORequest request = null;
-				WOResponse response = null;
-				boolean requestSucceeded = false;
-
-				if( aString != null && aString.length() > 0 ) {
-					aHeadersDict.setObjectForKey( new NSMutableArray<>( aString ), "filepath" );
-				}
-				if( showFiles ) {
-					aHeadersDict.setObjectForKey( new NSMutableArray<>( "YES" ), "showFiles" );
-				}
-
-				request = new WORequest( MObject._GET, RemoteBrowseClient.getPathString, MObject._HTTP1, aHeadersDict, null, null );
+				final WOHTTPConnection anHTTPConnection = new WOHTTPConnection( host.name(), WOApplication.application().lifebeatDestinationPort() );
 				anHTTPConnection.setReceiveTimeout( 5000 );
 
-				requestSucceeded = anHTTPConnection.sendRequest( request );
+				final NSMutableDictionary<String, NSMutableArray<String>> headers = (NSMutableDictionary<String, NSMutableArray<String>>)WOTaskdHandler.siteConfig().passwordDictionary().mutableClone();
+
+				if( path != null && path.length() > 0 ) {
+					headers.put( "filepath", new NSMutableArray<>( path ) );
+				}
+
+				if( showFiles ) {
+					headers.put( "showFiles", new NSMutableArray<>( "YES" ) );
+				}
+
+				final WORequest request = new WORequest( MObject._GET, RemoteBrowseClient.getPathString, MObject._HTTP1, headers, null, null );
+
+				WOResponse response = null;
+				boolean requestSucceeded = anHTTPConnection.sendRequest( request );
 
 				if( requestSucceeded ) {
 					response = anHTTPConnection.readResponse();
 				}
 
 				if( response == null || !requestSucceeded || response.status() != 200 ) {
-					throw new MonitorException( "Error requesting directory listing for " + aString + " from " + aHost.name() );
+					throw new MonitorException( "Error requesting directory listing for " + path + " from " + host.name() );
 				}
 
 				try {
-					aFileListDictionary = _getFileListOutOfResponse( response, aString );
+					aFileListDictionary = _getFileListOutOfResponse( response, path );
 				}
 				catch( MonitorException me ) {
 					if( NSLog.debugLoggingAllowedForLevelAndGroups( NSLog.DebugLevelCritical, NSLog.DebugGroupDeployment ) ) {
@@ -235,17 +240,17 @@ public class FileBrowser extends MonitorComponent {
 					throw me;
 				}
 
-				aHost.isAvailable = true;
+				host.isAvailable = true;
 			}
 			catch( MonitorException me ) {
-				aHost.isAvailable = true;
+				host.isAvailable = true;
 				throw me;
 			}
 			catch( Exception localException ) {
-				aHost.isAvailable = false;
+				host.isAvailable = false;
 				NSLog.err.appendln( "Exception requesting directory listing: " );
 				localException.printStackTrace();
-				throw new MonitorException( "Exception requesting directory listing for " + aString + " from " + aHost.name() + ": " + localException.toString() );
+				throw new MonitorException( "Exception requesting directory listing for " + path + " from " + host.name() + ": " + localException.toString(), localException );
 			}
 
 			return aFileListDictionary;
